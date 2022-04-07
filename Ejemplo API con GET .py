3674b19c1a8438e9c8a8b1840bfe79cebb731f6c
LeandroAgustnCastillo <85946312+LeandroAgustnCastillo@users.noreply.github.com>
2022-04-07 20:27:19 -0300

    Update Ejemplo API con GET .py

--- Ejemplo API con GET .py
+++ Ejemplo API con GET .py
@@ -1,3 +1,16 @@
 import requests
 
 resp = requests.get('https://openlibrary.org/works/OL45883W.json')
+
+
+import os
+
+
+//Código para verificarr:
+  
+import os
+
+file = open("/ruta/filename.txt", "w")
+file.write("Primera línea" + os.linesep)
+file.write("Segunda línea")
+file.close()
